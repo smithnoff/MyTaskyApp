62a40dd8e14c5c53f2b0e294f2d7641671b3afeb
Cesar Smith <ingcesarsmith@gmail.com>
2023-03-19 22:36:03 -0300

    change repository name and impl

--- app/src/main/java/com/smithnoff/mytaskyapp/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/smithnoff/mytaskyapp/data/repository/AuthRepositoryImpl.kt
@@ -1,10 +1,10 @@
 package com.smithnoff.mytaskyapp.data.repository
 
 import com.smithnoff.mytaskyapp.data.remote.TaskyApi
-import com.smithnoff.mytaskyapp.domain.repository.UserRepository
+import com.smithnoff.mytaskyapp.domain.repository.AuthRepository
 import javax.inject.Inject
 
-class UserRepositoryImpl @Inject constructor(private val api: TaskyApi) : UserRepository {
+class AuthRepositoryImpl @Inject constructor(private val api: TaskyApi) : AuthRepository {
 
     override suspend fun registerUser() {
 
--- app/src/main/java/com/smithnoff/mytaskyapp/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/smithnoff/mytaskyapp/domain/repository/AuthRepository.kt
@@ -1,6 +1,6 @@
 package com.smithnoff.mytaskyapp.domain.repository
 
-interface UserRepository {
+interface AuthRepository {
     suspend fun registerUser()
     suspend fun loginUser()
 }
